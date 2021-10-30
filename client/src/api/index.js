00302a4a46b5be14311e3dc0f9e6312c6cd94bb8
Mace <66080729+MaceGood@users.noreply.github.com>
2021-10-30 20:12:06 +0200

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://travel-the-world0.herokuapp.com/" });
 
 const user = localStorage.getItem("user");
 API.interceptors.request.use(
